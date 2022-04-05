ad97d8759ef6fc6155f3cae23d45d5d1cd274cd5
CmmSheparD <ustynow.gleb@gmail.com>
2022-04-06 01:41:06 +0300

    feat: PIN-code generating module.

---
+++ src/generator.py
@@ -0,0 +1,13 @@
+from random import randint
+
+
+def get_pin_code(length: int = 4) -> str:
+	pin = []
+	for i in range(length):
+		pin.append(str(randint(0, 9)))
+	return ''.join(pin)
+
+
+if __name__ == '__main__':
+	print(get_pin_code())
+	print(get_pin_code(6))
