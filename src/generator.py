bbf4fc2ce8cb63f4957d756d721dd185e7d4cfec
CmmSheparD <ustynow.gleb@gmail.com>
2022-07-28 02:37:32 +0300

    chore: Removed default argument value, module now generates 8-digit code as a scropt.

--- src/generator.py
+++ src/generator.py
@@ -1,13 +1,9 @@
 from random import randint
 
 
-def get_pin_code(length: int = 4) -> str:
-    pin = []
-    for i in range(length):
-        pin.append(str(randint(0, 9)))
-    return ''.join(pin)
+def get_pin_code(length: int) -> str:
+    return ''.join((str(randint(0, 9)) for x in range(length)))
 
 
 if __name__ == '__main__':
-    print(get_pin_code())
-    print(get_pin_code(6))
+    print(get_pin_code(8))
