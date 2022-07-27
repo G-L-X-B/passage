80c918c0b5fa540d95ec096344e28fbec97ca5f9
CmmSheparD <ustynow.gleb@gmail.com>
2022-07-28 02:34:03 +0300

    chore: Indentation fix.

--- src/generator.py
+++ src/generator.py
@@ -2,12 +2,12 @@ from random import randint
 
 
 def get_pin_code(length: int = 4) -> str:
-	pin = []
-	for i in range(length):
-		pin.append(str(randint(0, 9)))
-	return ''.join(pin)
+    pin = []
+    for i in range(length):
+        pin.append(str(randint(0, 9)))
+    return ''.join(pin)
 
 
 if __name__ == '__main__':
-	print(get_pin_code())
-	print(get_pin_code(6))
+    print(get_pin_code())
+    print(get_pin_code(6))
--- src/main.py
+++ src/main.py
@@ -5,20 +5,20 @@ from generator import get_pin_code
 
 
 class Application(ttk.Frame):
-	def __init__(self, master = None):
-		super().__init__(master)
-		self.grid()
-		self.master.title('passage')
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.grid()
+        self.master.title('passage')
 
-		ttk.Label(self, text='Press button to create a pin code').grid(column=0, row=0)
-		self.pin = tk.StringVar()
-		self.pin_label = ttk.Label(self, textvariable=self.pin).grid(column=0, row=1)
-		ttk.Button(self, text='Get', command=self.generate_pin_code).grid(column=0, row=2)
+        ttk.Label(self, text='Press button to create a pin code').grid(column=0, row=0)
+        self.pin = tk.StringVar()
+        self.pin_label = ttk.Label(self, textvariable=self.pin).grid(column=0, row=1)
+        ttk.Button(self, text='Get', command=self.generate_pin_code).grid(column=0, row=2)
 
-	def generate_pin_code(self):
-		self.pin.set(get_pin_code())
+    def generate_pin_code(self):
+        self.pin.set(get_pin_code())
 
 
 if __name__ == '__main__':
-	app = Application()
-	app.mainloop()
+    app = Application()
+    app.mainloop()
